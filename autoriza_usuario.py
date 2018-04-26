19eaa3a8f943d9cd4414b74d9f8a51933d6396fe
eduardo_quadros@farias.com.br <Edu056585>
2018-04-25 21:19:50 -0300

    first commit

---
+++ autoriza_usuario.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import tweepy
+from tweepy import OAuthHandler
+
+CONSUMER_API_KEY = 'kEzxJsRPtzfqnvUd0huaSKluU'
+CONSUMER_API_SECRET = 'snaNX6ThskkuJMZGPbCHdoOJq3Vl5UOcc9pbtEs95kGGtchBWQ'
+ACCESS_TOKEN = '169315101-bRamGG8OLre98TYuuffR2emOd270aFA1r2sI0FTM'
+ACCESS_TOKEN_SECRET = 'ivsdVTu4QeUyAikyE6TVMQN5AxIG5VNTIATW3Acwz55co'
+
+auth = OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+if (not api):
+   print('Ocorreu um erro ao tentar conectar!')
+
